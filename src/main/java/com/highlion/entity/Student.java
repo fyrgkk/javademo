85ba17a3984c233e0c6576dad71fd982f0bed3f8
gaokangkang <gkk15711255657@163.com>
2019-01-29 17:14:00 +0800

    410522424

--- src/main/java/com/highlion/entity/Student.java
+++ src/main/java/com/highlion/entity/Student.java
@@ -1,5 +1,9 @@
 package com.highlion.entity;
 
 public class Student {
-
+ public void test() {
+	 System.out.println("---------");
+	 System.out.println("++++++++++");
+	 System.out.println("AAAAAAAAA");
+ }
 }
