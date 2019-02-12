9a634fb3af3e7415f24ac789a0664da5dcb04e37
gaokangkang <410522424@qq.com>
2019-02-12 14:03:33 +0800

    Cat set get方法

--- src/main/java/com/highlion/entity/Cat.java
+++ src/main/java/com/highlion/entity/Cat.java
@@ -3,4 +3,17 @@ package com.highlion.entity;
 public class Cat {
 private int id;
 private int name;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public int getName() {
+	return name;
+}
+public void setName(int name) {
+	this.name = name;
+}
+
 }
