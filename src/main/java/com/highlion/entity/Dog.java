1ae8007e8581f13465e650007f5600bede5e13ee
gaokangkang <410522424@qq.com>
2019-02-13 08:54:34 +0800

    修改Dog

--- src/main/java/com/highlion/entity/Dog.java
+++ src/main/java/com/highlion/entity/Dog.java
@@ -1,7 +1,28 @@
 package com.highlion.entity;
 
 public class Dog {
+	private int id;
+	private String name;
 public Dog() {
 	// TODO Auto-generated constructor stub
 }
+public Dog(int id, String name) {
+	super();
+	this.id = id;
+	this.name = name;
+}
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+
+
 }
